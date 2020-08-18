5765ad304d247b1840a56a5eb3391d0c845301df
ouyanglinxia <ouyanglinxia@xiaomi.com>
2020-08-18 10:35:51 +0800

    提交route

--- lib/main.dart
+++ lib/main.dart
@@ -5,159 +5,70 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(title: '豆瓣电影'),
-    );
+        title: "Flutter Demo",
+        theme: ThemeData(
+          primaryColor: Colors.blue,
+        ),
+        initialRoute: '/First',
+        routes: {
+          '/First': (context) => FirstPage(),
+          "/Second": (context) => SecondPage()
+        },
+        home: FirstPage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-  final List<Text> _widgetItems = [Text('热映'), Text('找片'), Text('我的')];
-
+class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
-        body: Center(
-          child: _widgetItems[_selectedIndex],
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              title: Text('热映'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.panorama_fish_eye),
-              title: Text('找片'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.people),
-              title: Text('我的'),
-            )
-          ],
-          currentIndex: _selectedIndex,
-          fixedColor: Colors.blue,
-          type: BottomNavigationBarType.fixed,
-          onTap: _onItemTapped,
-        ));
+      appBar: AppBar(
+        title: Text("Route -- FirstPage"),
+      ),
+      body: RaisedButton(
+        child: Text("JUMP SecondRoute"),
+        onPressed: () {
+          _jumpToSecondPage(context);
+        },
+      ),
+    );
   }
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index; //刷新界面
-    });
+  _jumpToSecondPage(BuildContext context) async {
+    var passArgumnets = await Navigator.pushNamed(context, '/Second',
+        arguments: PassArgumnets('Data from FirstPage Navigator.pushNamed()'));
+    print('here');
+    print(passArgumnets);
   }
 }
 
-// TapboxA 管理自身状态.
-
-//------------------------- TapboxA ----------------------------------
-
-class TapboxA extends StatefulWidget {
-  TapboxA({Key key}) : super(key: key);
-
+class SecondPage extends StatelessWidget {
   @override
-  _TapboxAState createState() => new _TapboxAState();
-}
-
-class _TapboxAState extends State<TapboxA> {
-  bool _active = false;
-
-  void _handleTap() {
-    setState(() {
-      _active = !_active;
-    });
-  }
-
   Widget build(BuildContext context) {
-    return new GestureDetector(
-      onTap: _handleTap,
-      child: new Container(
-        child: new Center(
-          child: new Text(
-            _active ? 'Active' : 'Inactive',
-            style: new TextStyle(fontSize: 32.0, color: Colors.white),
-          ),
-        ),
-        width: 200.0,
-        height: 200.0,
-        decoration: new BoxDecoration(
-          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
-        ),
+    final PassArgumnets passArgumnets =
+        ModalRoute.of(context).settings.arguments;
+
+    print(passArgumnets.content);
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Route -- SecondPage"),
+      ),
+      body: RaisedButton(
+        child: Text("Go back!"),
+        onPressed: () {
+          Navigator.pop(context, PassArgumnets('Return Data from SecondPage'));
+        },
       ),
     );
   }
 }
 
-// ParentWidget 为 TapboxB 管理状态.
+class PassArgumnets {
+  String content;
 
-//------------------------ ParentWidget --------------------------------
-
-class ParentWidget extends StatefulWidget {
-  @override
-  _ParentWidgetState createState() => new _ParentWidgetState();
-}
-
-class _ParentWidgetState extends State<ParentWidget> {
-  bool _active = false;
-
-  void _handleTapboxChanged(bool newValue) {
-    setState(() {
-      _active = newValue;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-      child: new TapboxB(
-        active: _active,
-        onChanged: _handleTapboxChanged,
-      ),
-    );
-  }
-}
-
-//------------------------- TapboxB ----------------------------------
-
-class TapboxB extends StatelessWidget {
-  TapboxB({Key key, this.active: false, @required this.onChanged})
-      : super(key: key);
-
-  final bool active;
-  final ValueChanged<bool> onChanged;
-
-  void _handleTap() {
-    onChanged(!active);
-  }
-
-  Widget build(BuildContext context) {
-    return new GestureDetector(
-      onTap: _handleTap,
-      child: new Container(
-        child: new Center(
-          child: new Text(
-            active ? 'Active' : 'Inactive',
-            style: new TextStyle(fontSize: 32.0, color: Colors.white),
-          ),
-        ),
-        width: 200.0,
-        height: 200.0,
-        decoration: new BoxDecoration(
-          color: active ? Colors.lightGreen[700] : Colors.grey[600],
-        ),
-      ),
-    );
-  }
+  PassArgumnets(this.content);
 }
